1cf801d9fff5b51f26d3ad74d718fc3073dc2896
jopema <147610470+jopemaa@users.noreply.github.com>
2024-10-07 13:33:09 +0200

    Add files via upload

---
+++ ACTIVIDAD1.java
@@ -0,0 +1,15 @@
+public class ACTIVIDAD1 {
+
+    public static void main(String[] args) {
+        //VARIABLE HORA
+        final int horas = 1;
+        //VARIABLE MINUTOS
+        final int minutos = 42;
+        //VARIABLE SEGUNDOS
+        final int segundos = 12;
+        //SUMA
+        int totalsegundos = (horas * 3600) + (minutos * 60) + segundos;
+        //MOSTRAR RESULTADO
+        System.out.println(horas + " " + "horas" + " " + minutos + " " + "minutos" + " " + "y" + " " + segundos + " " + "segundos" + " " + "son" + " " + totalsegundos);
+    }
+}
---
+++ HOLAMUNDO.java
@@ -0,0 +1,8 @@
+public class HOLAMUNDO {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+
+    }
+}
